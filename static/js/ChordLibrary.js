2eb0ef7f67417a804fd12fe2312ecacc9e38a021
anuvette <thisisanuxoxo@gmail.com>
2023-10-11 07:00:14 +0545

    chord library expanded

--- static/js/ChordLibrary.js
+++ static/js/ChordLibrary.js
@@ -39,19 +39,15 @@ var ChordLibrary = {
 
  
     [JSON.stringify([Intervals.unison, Intervals.major_second, Intervals.perfect_fifth])]: "sus2",
-   // [JSON.stringify([Intervals.unison, Intervals.perfect_fifth, Intervals.major_second,])]: "sus2",
     [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.perfect_fifth])]: "m",
-  //  [JSON.stringify([Intervals.unison, Intervals.perfect_fifth, Intervals.minor_third])]: "m",
     [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.perfect_fifth])]: "",
- //   [JSON.stringify([Intervals.unison, Intervals.perfect_fifth, Intervals.major_third])]: "",
     [JSON.stringify([Intervals.unison, Intervals.perfect_fourth, Intervals.perfect_fifth])]: "sus4",
-  //  [JSON.stringify([Intervals.unison, Intervals.perfect_fifth, Intervals.perfect_fourth])]: "sus4",
     [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.flat_fifth])]: "dim",
- //   [JSON.stringify([Intervals.unison, Intervals.flat_fifth, Intervals.minor_third])]: "dim",
     [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.sharp_fifth])]: "aug",
     [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.sixth])]: "6(no5)",
     [JSON.stringify([Intervals.unison, Intervals.perfect_fifth, Intervals.sixth])]: "6(no3)",
     [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.sixth])]: "m6(no5)",
+    [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.sharp_fifth])]: "mb6(no5)",
     [JSON.stringify([Intervals.unison, Intervals.major_second, Intervals.sixth])]: "6sus2",
     [JSON.stringify([Intervals.unison, Intervals.perfect_fourth, Intervals.sixth])]: "6sus4",
     [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.sharp_fifth])]: "mb6",
@@ -65,6 +61,11 @@ var ChordLibrary = {
     [JSON.stringify([Intervals.unison, Intervals.perfect_fourth, Intervals.major_seventh])]: "maj7sus4(no5)",
     [JSON.stringify([Intervals.unison, Intervals.major_second, Intervals.flat_seventh])]: "7sus2(no5)",
     [JSON.stringify([Intervals.unison, Intervals.perfect_fourth, Intervals.flat_seventh])]: "7sus4(no5)",
+    [JSON.stringify([Intervals.unison, Intervals.major_seventh, Intervals.flat_seventh])]: "7,maj7(no3)",
+    [JSON.stringify([Intervals.unison, Intervals.flat_ninth, Intervals.major_seventh])]: "maj7b9(no3)",
+    [JSON.stringify([Intervals.unison, Intervals.major_second, Intervals.flat_ninth])]: "sus2(addb9)",
+
+
 
     [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.flat_ninth])]: "addb9(no5)",
     [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.flat_ninth])]: "maddb9(no5)", 
@@ -78,9 +79,40 @@ var ChordLibrary = {
 
     [JSON.stringify([Intervals.unison, Intervals.major_seventh, Intervals.thirteenth])]: "maj13(no3)",
     [JSON.stringify([Intervals.unison, Intervals.flat_seventh, Intervals.thirteenth])]: "13(no3)",
+    [JSON.stringify([Intervals.unison, Intervals.flat_seventh, Intervals.sharp_fifth])]: "b13(no3)",
 
+
+    [JSON.stringify([Intervals.unison, Intervals.sharp_fifth, Intervals.flat_fifth])]: "b6,b5(no3)",
+    [JSON.stringify([Intervals.unison, Intervals.major_second, Intervals.sharp_eleven])]: "sus2(add#11)",   
+
+
+    [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.perfect_fifth, Intervals.sixth])]: "6",
+    [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.perfect_fifth, Intervals.sixth])]: "m6",
+    [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.sixth, Intervals.major_ninth])]: "6/9",
+    [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.sixth, Intervals.major_ninth])]: "m6/9",
     [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.perfect_fifth, Intervals.major_seventh])]: "maj7",
+    [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.perfect_fifth, Intervals.flat_seventh])]: "m7",
+    [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.perfect_fifth, Intervals.flat_seventh])]: "7",
+    [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.flat_fifth, Intervals.flat_seventh])]: "m7(b5)",
+    [JSON.stringify([Intervals.unison, Intervals.major_second, Intervals.perfect_fifth, Intervals.major_seventh])]: "maj7sus2",
+    [JSON.stringify([Intervals.unison, Intervals.major_second, Intervals.perfect_fifth, Intervals.flat_seventh])]: "7sus2",
+    [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.sixth, Intervals.eleventh])]: "(6/11)",
+    [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.flat_fifth, Intervals.sixth])]: "dim7",
+
+
+
+
     [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.perfect_fifth, Intervals.major_ninth])]: "add9",
+    [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.major_seventh, Intervals.major_ninth])]: "maj9",
+    [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.flat_seventh, Intervals.major_ninth])]: "9",
+    [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.flat_seventh, Intervals.major_ninth])]: "m9",
+    [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.major_seventh, Intervals.eleventh])]: "maj11",
+    [JSON.stringify([Intervals.unison, Intervals.minor_third, Intervals.flat_seventh, Intervals.eleventh])]: "m11",
+    [JSON.stringify([Intervals.unison, Intervals.perfect_fifth, Intervals.flat_seventh, Intervals.eleventh])]: "m11(no3)",
+    [JSON.stringify([Intervals.unison, Intervals.major_third, Intervals.major_seventh, Intervals.thirteenth])]: "maj13",
+
+
+
 
 
 
